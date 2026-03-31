36c9d0b73ec0edb1c0cd8de28cf28efbe0e3ddb0
Shiv ram <shivramchhonkar@gmail.com>
2026-03-31 17:11:55 +0530

    fix

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,14 +1,25 @@
 module.exports = {
-    apps: [
-      {
-        name: "echat-api",
-        cwd: "/var/www/eChat/server",
-        script: "src/index.js",
-        interpreter: "node",
-        env: {
-          NODE_ENV: "production",
-          PORT: 4040,
-        },
+  apps: [
+    {
+      name: "echat",
+      cwd: "/var/www/eChat",
+      script: "npm",
+      args: "start",
+      interpreter: "none",
+
+      env: {
+        NODE_ENV: "production",
       },
-    ],
-  };
+
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "500M",
+      restart_delay: 5000,
+
+      error_file: "/var/log/pm2/echat-error.log",
+      out_file: "/var/log/pm2/echat-out.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      merge_logs: true,
+    },
+  ],
+};
