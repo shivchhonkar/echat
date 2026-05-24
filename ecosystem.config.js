93fcb5aad94d9c61ec71e5608b82d3a8c609a8e9
Shiv ram <shivramchhonkar@gmail.com>
2026-05-24 16:54:14 +0530

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,9 @@ module.exports = {
   apps: [
     {
       name: "echat-web",
-
-      cwd: "/root/projects/echat/client",
-
-      script: "serve",
-      args: "-s dist -l 6001",
-
+      cwd: "./client",
+      script: "npm",
+      args: "start",
       env: {
         NODE_ENV: "production",
       },
